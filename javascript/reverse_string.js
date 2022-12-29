8d0fdab2721df0ffe9b7375ab8070b49f949baf2
Nicholas Galante <nicholasgalante94@gmail.com>
2022-12-29 12:22:58 -0500

    Decalre revereString function

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,19 +1,47 @@
-function reverseString(str) {
-  // type your code here
+function reverseString(string) {
+  const reversedString = [];
+  for (let i = 0; i < string.length; i++) {
+    reversedString.unshift(string[i]);
+  }
+  return reversedString.join('')
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 'ih'");
-  console.log("=>", reverseString("hi"));
+  console.log("TEST 1 - Expecting: 'ih'");
+  console.log("Result:", reverseString("hi"));
 
-  console.log("");
+  console.log("TEST 2 - Expecting: 'ybabtac'");
+  console.log("Result:", reverseString("catbaby"));
 
-  console.log("Expecting: 'ybabtac'");
-  console.log("=>", reverseString("catbaby"));
+  console.log("TEST 3 - Expecting: '!dlrow olleH'");
+  console.log("Result:", reverseString("Hello world!"));
+
+  console.log("TEST 4 - Expecting: '12345'");
+  console.log("Result:", reverseString("54321"));
 }
 
 module.exports = reverseString;
 
-// Please add your pseudocode to this file
+/* The Objective:
+Using javascript, write a function reverseString that reverses the characters in a string. This function will receive one argument, a string, and return the reversed string.
+
+Input: "Hello world!"
+Output: "!dlrow olleH"
+
+Note: For the sake of practicing our algorithmic problem solving, we won't be using any type of built-in reverse methods!
+*/
+
+
+/* Pseudocode:
+Declare function that takes in string argument (string)
+    create new empty array reversedString
+    iterate over each element of string
+    add each index to the front of reversedString (unshift)
+    return reversedString
+*/
+
+
+
 // And a written explanation of your solution
+
